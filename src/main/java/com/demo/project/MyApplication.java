eac92d194526f5e35e7d72e0dee81d4a7a942e73
“Rolin” <2259286198@qq.com>
2023-03-19 19:12:55 +0800

    上传下载接口完成

--- src/main/java/com/demo/project/MyApplication.java
+++ src/main/java/com/demo/project/MyApplication.java
@@ -1,14 +1,22 @@
 package com.demo.project;
 
+import com.demo.project.controller.CommonController;
+import org.apache.tomcat.util.http.fileupload.FileItem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import java.io.File;
 
 @SpringBootApplication
 @MapperScan("com.demo.project.mapper")
 public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
+        CommonController controller = new CommonController();
+        System.out.println("coming...");
     }
 
 }
